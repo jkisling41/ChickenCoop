0fa741d3462c4349381c99af035651b14e1da4a4
jkisling41 <jkisling41@gmail.com>
2021-04-27 12:10:57 -0500

    updated temptrigger and set temp ranges

--- ChickenCoop.py
+++ ChickenCoop.py
@@ -3,23 +3,45 @@ import RPi.GPIO as GPIO
 import dht11
 import time
 import datetime
+import SendEmail
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
-#setting relay 1 and 2 as output pins
-GPIO.setup(16, GPIO.OUT)
-GPIO.setup(20, GPIO.OUT)
-
-
 relay1 = 16 
 relay2 = 20
+#setting relay 1 and 2 as output pins
+GPIO.setup(relay1, GPIO.OUT)
+GPIO.setup(relay2, GPIO.OUT)
+
+#relay is backwards and this turns the pins off
 ON = 0
 OFF = 1
+GPIO.output(relay1,OFF)
+GPIO.output(relay2,OFF)
 
 def pin_trigger(pin,state):
     GPIO.output(pin, state)
+
+def tempTrigger(temp):
+    tooHot = 76
+    tooCold = 64
+    idealTemp = range(65,75,1)
+    extremeHot = 95
+    extremeCold = 50
+    if temp >= tooHot:
+        pin_trigger(relay1,ON)
+        #if temp >= extremeHot:
+            #SendEmail()
+    if temp <= tooCold:
+        pin_trigger(relay2,ON)
+        #if temp <= extremeCold:
+            #SendEmail()
+    elif temp == idealTemp:
+        pin_trigger(relay1,OFF)
+        pin_trigger(relay2,OFF)
+
 
 instance = dht11.DHT11(pin=17)
 
@@ -31,6 +53,8 @@ while True:
         print("Last valid input: " + str(datetime.datetime.now()))
         print("Temp: %d F" % (fahrenheit))
         print("Humidity: %d %%" % humidity)
+        tempTrigger(fahrenheit)
+        
     time.sleep(1)
 
 
